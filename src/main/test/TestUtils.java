26d77967a0ecab780a8ec53ba7f5dba3b8cd1090
paul-saan <paul.milleville@gmail.com>
2023-09-06 15:44:15 +0200

    Test TextReader et Utils V1

---
+++ src/main/test/TestUtils.java
@@ -0,0 +1,23 @@
+package main.test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import main.java.Utils;
+
+
+/**
+ * The function checks that the method correctly converts RGB colours to ANSI format.
+ */
+public class TestUtils {
+    @Test
+    public void testRGBToANSI() {
+        int[] rgb = {255, 0, 0};
+        String ansiRed = Utils.RGBToANSI(rgb, false);
+        assertEquals("\u001b[38;2;255;0;0m", ansiRed);
+        int[] rgbBackground = {0, 0, 255};
+        String ansiBlueBackground = Utils.RGBToANSI(rgbBackground, true);
+        assertEquals("\u001b[48;2;0;0;255m", ansiBlueBackground);
+    }
+
+}
